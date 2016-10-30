9c5de5ecdf4b5beb1d5e9b0e2e29093228b6b843
Arseny Sher <arsv.sher@gmail.com>
2016-10-30 15:41:23 +0300

    using ergo kbs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,10 +2,6 @@
 ;; See how to build it in Ubuntu here:
 ;; http://ubuntuhandbook.org/index.php/2014/10/emacs-24-4-released-install-in-ubuntu-14-04/
 
-;; M-insert now reloads init.el. Lambda here just passes closured load-file as a function
-(global-set-key [M-insert] '(lambda() (interactive) (load-file "~/.emacs.d/init.el")))
-;; TODO: save init.el automatically before reloading
-
 ;;________________________________________________
 ;; Packaging stuff
 ;; add package repos
@@ -77,22 +73,6 @@
 ;; autoreload all files from disk
 (global-auto-revert-mode 1)
 
-
-;;_________________________________________________
-;; Moving
-;; Moving like in ergoemacs
-(global-set-key (kbd "M-j") 'backward-char)
-(global-set-key (kbd "M-k") 'next-line)
-(global-set-key (kbd "M-l") 'forward-char)
-(global-set-key (kbd "M-i") 'previous-line)
-(global-set-key (kbd "M-o") 'forward-word)
-(global-set-key (kbd "M-u") 'backward-word)
-
-;; make C-d behave like backspace
-(global-set-key (kbd "\C-d") 'delete-backwards-char)
-
-
-
 ;;_________________________________________________
 ;; Text processing
 (transient-mark-mode 1) ; Highlight the region when the mark is active
@@ -113,7 +93,6 @@
   (next-line 1)
   (yank)
 )
-(global-set-key (kbd "\C-c\C-d") 'duplicate-line)
 
 ;; TODO: call to mind why I wasn't satisfied with out-of-the-box move-text package and decided to rewrite
 ;; these functions manually. Probably because of regions? 
@@ -158,26 +137,9 @@
 ;;   (interactive "*p")
 ;;   (move-text-internal (- arg)))
 
-;; (global-set-key [\C-\S-down] 'move-text-down)
-;; (global-set-key [\C-\S-up] 'move-text-up)
-(move-text-default-bindings) ; enable move text with default bindings, M-up M-down moves lines
-
 ;; enable comments like in idea
 (require 'comment-idea)
 ;; (load "comment-idea.el") ;; for debugging
-
-
-(global-set-key (kbd "C-;") 'comment-idea)
-;; ergoemacs-like deletions
-(global-set-key (kbd "M-d") 'backward-delete-char)
-(global-set-key (kbd "M-f") 'delete-forward-char)
-(global-set-key (kbd "M-e") 'backward-kill-word)
-(global-set-key (kbd "M-r") 'kill-word)
-;; ergoemacs-like set mark
-(global-set-key (kbd "M-<SPC>") 'cua-set-mark)
-;; ergoemacs-like goto start/end of line
-(global-set-key (kbd "M-h") 'move-beginning-of-line)
-(global-set-key (kbd "M-H") 'move-end-of-line)
 
 ;;______________________________________________________________
 ;; Windows and frames
@@ -204,8 +166,6 @@
 (defun my-split-root-window-right (&optional size)
   (interactive "P")
   (my-split-root-window size 'right))
-(global-set-key (kbd "\C-x4") 'my-split-root-window-below)
-(global-set-key (kbd "\C-x5") 'my-split-root-window-right)
 
 ;; open project tree on f8
 (require 'all-the-icons) ; load icons for 'icons regime
@@ -229,15 +189,6 @@
   (interactive)
   (remove-hook 'post-command-hook 'tail-f-msgs))
 (tail-messages-enable)
-
-
-;; enable switching windows with M-arrows
-(windmove-default-keybindings 'meta)
-;; ...and with M-vim arrows
-;; (global-set-key (kbd "M-h") 'windmove-left)
-;; (global-set-key (kbd "M-j") 'windmove-down)
-;; (global-set-key (kbd "M-k") 'windmove-up)
-;; (global-set-key (kbd "M-l") 'windmove-right)
 
 
 ;;____________________________________________________________
@@ -281,14 +232,6 @@
  (setq tramp-default-method "ssh")
 
 
-;;____________________________________________________________
-;; F key mappings
-(global-set-key [f1] help-map) ; help prefix
-(global-set-key [f7] 'desktop+-load) ; load saved desktop
-(global-set-key [f8] 'neotree-toggle) ; show and hide neotree
-(define-key global-map [f11] 'switch-fullscreen)
-(global-set-key [f12] 'dired-omit-mode) ; show and hide hidden files in dired
-
 ;;______________________________________
 ; Latex stuff
 ;;______________________________________
@@ -312,6 +255,80 @@
    (output-dvi "xdvi")
    (output-pdf "Okular")
    (output-html "xdg-open")))))
+
+
+;;____________________________________________________________
+;; Now, the keys.
+;; About prefixes:
+;; * Alt is the best
+;; * Ctrl is the second priority (right Alt)
+;; * Shift Alt is ok, if the key is for right hand, but pinky is getting nervious
+;; * Ctrl Alt is ok, if the key is on the home row or nearby. Ergo doesn't use it
+;; * Ctrl Shift doesn't work in terminals, avoid it for important keys!
+;; * Ctrl Alt Shift -- I reserved for system apps
+
+;; The home row:
+(global-set-key (kbd "C-a") 'mark-whole-buffer) ; mark whole buffer, ergo
+(global-set-key (kbd "M-a") 'execute-extended-command) ; run command, ergo
+(global-set-key (kbd "M-d") 'backward-delete-char)
+(global-set-key (kbd "M-f") 'delete-forward-char)
+(global-set-key (kbd "C-f") 'isearch-forward) ; ergo
+(global-set-key (kbd "C-M-f") 'isearch-backward)
+(global-set-key (kbd "M-F") 'occur) ; like grep
+(global-set-key (kbd "M-g") 'kill-line) ; kill the rest of the line; ergo
+(global-set-key (kbd "M-G") '(lambda () (interactive) (kill-line 0))) ; kill the line up to the cursor, ergo
+(global-set-key (kbd "M-h") 'move-beginning-of-line) ; ergo
+(global-set-key (kbd "M-H") 'move-end-of-line) ; ergo
+(global-set-key (kbd "M-j") 'backward-char) ; ergo
+(global-set-key (kbd "M-k") 'next-line) ; ergo
+(global-set-key (kbd "M-l") 'forward-char) ; ergo
+(global-set-key (kbd "C-l") 'goto-line) ; ergo
+(global-set-key (kbd "C-;") 'comment-idea)
+
+;; The qwerty row:
+(global-set-key (kbd "M-w") 'just-one-space) ; ergo
+(global-set-key (kbd "C-w") 'kill-this-buffer) ; ergo
+(global-set-key (kbd "M-e") 'backward-kill-word) ; ergo
+(global-set-key (kbd "M-r") 'kill-word) ; ergo
+(global-set-key (kbd "C-M-r") 'revert-buffer)
+(global-set-key (kbd "C-r") 'query-replace)
+(global-set-key (kbd "M-u") 'backward-word) ; ergo
+(global-set-key (kbd "M-i") 'previous-line) ; ergo
+(global-set-key (kbd "M-o") 'forward-word) ; ergo
+(global-set-key (kbd "M-p") 'recenter-top-bottom) ; ??? ergo
+
+;; Space row
+(global-set-key (kbd "M-<SPC>") 'cua-set-mark) ; ergo
+;;____________________
+
+(global-set-key (kbd "\C-c\C-d") 'duplicate-line)
+
+(global-set-key (kbd "\C-x4") 'my-split-root-window-below)
+(global-set-key (kbd "\C-x5") 'my-split-root-window-right)
+
+;; (global-set-key [\C-\S-down] 'move-text-down)
+;; (global-set-key [\C-\S-up] 'move-text-up)
+(move-text-default-bindings) ; enable move text with default bindings, M-up M-down moves lines
+
+;; enable switching windows with M-arrows
+(windmove-default-keybindings 'meta)
+;; ...and with M-vim arrows
+;; (global-set-key (kbd "M-h") 'windmove-left)
+;; (global-set-key (kbd "M-j") 'windmove-down)
+;; (global-set-key (kbd "M-k") 'windmove-up)
+;; (global-set-key (kbd "M-l") 'windmove-right)
+
+;;____________________________________________________________
+;; F key mappings
+(global-set-key [f1] help-map) ; help prefix
+(global-set-key [f7] 'desktop+-load) ; load saved desktop
+(global-set-key [f8] 'neotree-toggle) ; show and hide neotree
+(define-key global-map [f11] 'switch-fullscreen)
+(global-set-key [f12] 'dired-omit-mode) ; show and hide hidden files in dired
+
+;; M-insert now reloads init.el. Lambda here just passes closured load-file as a function
+(global-set-key [M-insert] '(lambda() (interactive) (load-file "~/.emacs.d/init.el")))
+;; TODO: save init.el automatically before reloading
 
 ;; TODO list:
 ;; cheatsheet in txt
