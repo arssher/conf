194b792eb6996604e731a368c4d66e73e368d95d
Arseny Sher <arsv.sher@gmail.com>
2017-02-02 08:36:40 +0300

    rough flycheck setup

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -291,8 +291,31 @@
 ;;____________________________________________________________
 ;; Syntax checking and other checks on the fly
 
-;; enable flychecker TODO
-;; (add-hook 'after-init-hook #'global-flycheck-mode)
+;; enable flychecker
+(global-flycheck-mode)
+;; Here we can disable some checkers.
+;; It is a buffer-local variable, but using setq-default I set it's value
+;; globally.
+(setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck))
+;; set clang executable path
+(setq flycheck-c/c++-clang-executable "clang-3.8")
+;; Probably you will also want to put in the .dir-locals.el file of the root of
+;; you project something like
+;; ((c-mode
+;;   (flycheck-clang-include-path . (
+;; 				  "/home/ars/postgres/postgresql/src/include"
+;; 				  "/home/ars/tmp/tmp/postgresql_build/src/include"
+;; 				  "/home/ars/postgres/reverse_executor_test_extension/src/include"
+;; 				  ))
+;;   (flycheck-clang-args . (
+;; 			  "-Wignored-attributes"
+;; 			  ))
+;;   (flycheck-gcc-include-path . (
+;; 				  "/home/ars/postgres/postgresql/src/include"
+;; 				  "/home/ars/tmp/tmp/postgresql_build/src/include"
+;; 				  "/home/ars/postgres/reverse_executor_test_extension/src/include"
+;; 				  ))
+;; ))
 
 ;; highlight changed lines
 (require 'diff-hl-flydiff)
--- emacs/.emacs.d/static_packages/my-misc.el
+++ emacs/.emacs.d/static_packages/my-misc.el
@@ -2,9 +2,28 @@
 
 ;; Source: http://www.emacswiki.org/emacs-en/download/misc-cmds.el
 (defun revert-buffer-no-confirm ()
-    "Revert buffer without confirmation."
+    "Revert buffer without confirmation, and then reread local-dir vars."
     (interactive)
-    (revert-buffer :ignore-auto :noconfirm))
+    (revert-buffer :ignore-auto :noconfirm)
+    (reload-dir-locals-for-all-buffer-in-this-directory))
+
+;; taken from
+;; http://emacs.stackexchange.com/questions/13080/reloading-directory-local-variables
+(defun reload-dir-locals-for-current-buffer ()
+  "reload dir locals for the current buffer"
+  (interactive)
+  (let ((enable-local-variables :all))
+    (hack-dir-local-variables-non-file-buffer)))
+
+(defun reload-dir-locals-for-all-buffer-in-this-directory ()
+  "For every buffer with the same `default-directory` as the
+current buffer's, reload dir-locals."
+  (interactive)
+  (let ((dir default-directory))
+    (dolist (buffer (buffer-list))
+      (with-current-buffer buffer
+        (when (equal default-directory dir))
+        (reload-dir-locals-for-current-buffer)))))
 
 (message "my-misc loaded")
 (provide 'my-misc)
