8d2a928b446d4b0f6f6d93ea92cbf545a857bf06
Arseny Sher <sher-ars@yandex.ru>
2018-04-21 17:48:49 +0300

    fast version

--- bash/.bash_scripts/bin/sort_mod_time.sh
+++ bash/.bash_scripts/bin/sort_mod_time.sh
@@ -27,11 +27,13 @@ if [[ $# != 0 ]]; then
 fi
 
 # will break if newlines in files
+offset=0
 find "${path}" -type d | while read d; do
     echo "Processing $d"
     find "${d}" -type f | sort | while read f; do
-	echo "Processing file $f"
-	touch "${f}"
-	sleep 1
+	modtime=$(date --date "${offset} hours")
+	echo "Setting mod time of file $f to $modtime"
+	touch -d "${modtime}" "${f}"
+	let "offset=offset+1"
     done
 done
