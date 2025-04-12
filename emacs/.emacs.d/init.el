1ee195cc0fd457f89ad129ab67b03065dbc4cf5c
Arseny Sher <sher-ars@yandex.ru>
2025-04-12 11:10:30 +0300

    modernize emacs c-w

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -129,7 +129,7 @@
 
 ;; The qwerty row:
 (global-set-key (kbd "M-w") 'select-line) ; ergo
-(global-set-key (kbd "C-w") 'kill-this-buffer) ; ergo
+(global-set-key (kbd "C-w") 'kill-current-buffer) ; ergo
 (global-set-key (kbd "C-M-w") 'just-one-space) ; ergo
 (global-set-key (kbd "M-e") 'backward-kill-word) ; ergo
 (global-set-key (kbd "M-r") 'kill-word) ; ergo
